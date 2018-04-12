2c2d41316c829949f73ffa42e7a24aed1a58f0a3
emmadema <emma.demango@gmail.com>
2018-04-12 16:22:10 -0600

    adding comp

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import './App.css';
 
 const App = () => {
-  return (<div className = "App">Hello World!</div>);
+  return (<div className = "App">Yogis</div>);
 };
 
 export default App;
